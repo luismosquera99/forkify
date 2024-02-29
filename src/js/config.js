5849b510b81488d68e37678ca90ad229ba0ad38f
luismosquera99 <lucho9975@gmail.com>
2024-02-28 22:23:41 -0500

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '67bc179e-1b46-46e8-a3c2-177a7f7821b4';
+export const MODAL_CLOSE_SEC = 2.5;
